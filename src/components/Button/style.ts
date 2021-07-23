4feae9d46daa21521e53f697ca2a2c2dcc6dd91f
Jean <jeanrgl03@gmail.com>
2021-07-23 11:33:02 -0300

    Definição de componente Button

---
+++ src/components/Button/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+    background: #ff9000;
+    height: 56px;
+    border-radius: 10px;
+    border: 0;
+    color: #312e38;
+    width: 50%;
+    margin-top: 16px;
+
+    
+    &:hover {
+        background: #ff900095;
+        transition: 0.2s;
+    }
+`;
