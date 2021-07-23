c1a6a58aa747a4cddb0952a22da917f894771f41
Jean <jeanrgl03@gmail.com>
2021-07-23 15:42:09 -0300

    Inserção de estilização em input conforme foco

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,44 @@
-import React, { InputHTMLAttributes } from "react";
-import { Container } from "./style";
+import React, { InputHTMLAttributes, useEffect, useState, useCallback, useRef } from "react";
+import { Container } from "./style"
 import { IconBaseProps } from 'react-icons'
+import { useField } from '@unform/core'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
     icon?: React.ComponentType<IconBaseProps>
 }
-const Input: React.FC<InputProps> = ({icon: Icon, ...rest}) => (
-    <Container>
-        {Icon && <Icon size={16}/>}
-        <input {...rest}/>
-    </Container>
-);
+const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
+    const inputRef = useRef<HTMLInputElement>(null);
 
+    const [isFocused, setFocus] = useState(false);
+    const [isFilled, SetFill] = useState(false);
+
+    const { fieldName, defaultValue, error, registerField } = useField(name);
+
+    const handleInputBlur = useCallback(() => {
+        setFocus(false);
+
+        SetFill(!! inputRef.current?.value);
+        console.log(inputRef)
+    }, []);
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: "value",
+        });
+    }, [ fieldName, registerField]);
+    return (
+        <Container isFocused={isFocused} isFilled={isFilled}>
+            {Icon && <Icon size={16}/>}
+            <input onFocus={() => setFocus(true)} 
+                   onBlur={handleInputBlur}
+                   defaultValue={defaultValue}
+                   ref={inputRef}
+                    {...rest}/>
+                    {error}
+        </Container>
+    );
+}
 export default Input;
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FiArrowLeft, FiLock, FiUser } from 'react-icons/fi'
 import { Container, Content, Background } from './style'
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import { Form } from '@unform/web'
+import { FormHandles } from '@unform/core'
+import { useCallback } from "react";
 
-const SignUp: React.FC = () => (
-    <Container>
-        <Background/>
+const SignUp: React.FC = () => {
 
-        <Content>
-            <form action="">
-                <h1>Faça seu cadastro</h1>
+    const formRef = useRef<FormHandles>(null);
 
-                <Input icon={ FiUser } name="email" placeholder="E-mail" />
-                <Input icon={ FiLock } name="password" type="password" placeholder="Senha" />
+    const handleSubmit = useCallback((data: Object): void => {
+        console.log(data);
+    },[]);
 
-                <Button type="submit"> Cadastrar </Button>
-            </form>
+    return (
+        <Container>
+            <Background/>
 
-            <a href="teste"><FiArrowLeft/> Voltar para o login</a>
-        </Content>
-    </Container>
-);
+            <Content>
+                <Form 
+                ref={formRef} 
+                onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
+
+                    <Input icon={ FiUser } name="email" placeholder="E-mail" />
+                    <Input icon={ FiLock } name="password" type="password" placeholder="Senha" />
+
+                    <Button type="submit"> Cadastrar </Button>
+                </Form>
+
+                <a href="teste"><FiArrowLeft/> Voltar para o login</a>
+            </Content>
+        </Container>
+    )};
 export default SignUp;
