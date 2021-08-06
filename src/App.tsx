a8224963348de61daa7bf362288aafd00c6148c3
Jean <jeanrgl03@gmail.com>
2021-08-06 11:33:19 -0300

    Refatoração para inserção de direcionamento

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import SignIn from './pages/signin'
 import Global from './style/global';
 //import Toast from './components/Toast';
-import AppProvider from './hooks';
+import Dashboard from './pages/Dashboard';
+import AuthProvider from './hooks';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
     <>
-      <AppProvider>
-        <SignIn/>
-      </AppProvider>
-      
-      <Global/>
+      <Router>
+        <AuthProvider>
+          <Routes/>
+        </AuthProvider>
+        
+        <Global/>
+      </Router>
     </>
   );
 }
